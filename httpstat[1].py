ac47e4623fa35e5ac57329004b6de821c3d7b11e
YuanhaoJin <jinyuanh@bu.edu>
2017-09-15 22:04:27 -0400

    Update httpstat[1].py
    Change the appearance of https_template

--- httpstat[1].py
+++ httpstat[1].py
@@ -67,11 +67,11 @@ curl_format = """{
 https_template = """
   DNS Lookup   TCP Connection   TLS Handshake   Server Processing   Content Transfer
 [   {a0000}  |     {a0001}    |    {a0002}    |      {a0003}      |      {a0004}     ]
-             |                |               |                   |                  |
-    namelookup:{b0000}        |               |                   |                  |
-                        connect:{b0001}       |                   |                  |
-                                    pretransfer:{b0002}           |                  |
-                                                      starttransfer:{b0003}          |
+|            |                |               |                   |                  |
+|   namelookup:{b0000}        |               |                   |                  |
+|                       connect:{b0001}       |                   |                  |
+|                                  pretransfer:{b0002}           |                   |
+|                                                   starttransfer:{b0003}            |
                                                                                  total:{b0004}
 """[1:]
 
